a0e36b7acb2d04475a6ad58b4dda177a29d230e8
Dario <dariomrkonjic1@gmail.com>
2022-11-13 12:41:23 +0100

    Removed GenerateButton and updated OutputField

--- src/app.tsx
+++ src/app.tsx
@@ -17,13 +17,22 @@ const App = () => {
         length : 12,
         showPassword: true,
         generatedPassword: "",
+        canGenerate : true,
     });
 
     return (
         <Fragment>
             <div>
-                <OutputField output={mutable.current.generatedPassword} isShown={true} />
-                <GenerateButton callback={() => {
+                <OutputField
+                output={mutable.current.generatedPassword}
+                isShown={mutable.current.showPassword}
+                canGenerate={
+                    mutable.current.useUppercase ||
+                    mutable.current.useLowercase ||
+                    mutable.current.useNumbers ||
+                    mutable.current.useSpecial
+                }
+                callback={() => {
                     mutable.current.generatedPassword = generator(
                         mutable.current.useUppercase,
                         mutable.current.useLowercase,
@@ -32,7 +41,7 @@ const App = () => {
                         mutable.current.length
                     )
                     reRender();
-                }} isDisabled={false} text="Generate" />
+                }}/>
             </div>
             <div>
                 <LengthSlider callback={(value) => {
@@ -40,15 +49,19 @@ const App = () => {
                 }} defaultValue={mutable.current.length} min={8} max={128} />
                 <ToggleButton callback={(state) => {
                     mutable.current.useUppercase = state;
+                    reRender();
                 }} baseState={mutable.current.useUppercase} text="A-Z" />
                 <ToggleButton callback={(state) => {
                     mutable.current.useLowercase = state;
+                    reRender();
                 }} baseState={mutable.current.useLowercase} text="a-z" />
                 <ToggleButton callback={(state) => {
                     mutable.current.useNumbers = state;
+                    reRender();
                 }} baseState={mutable.current.useNumbers} text="0-9" />
                 <ToggleButton callback={(state) => {
                     mutable.current.useSpecial = state;
+                    reRender();
                 }} baseState={mutable.current.useSpecial} text="Special" />
                 <ToggleButton callback={(state) => {
                     mutable.current.showPassword = state;
--- src/components/outputfield.tsx
+++ src/components/outputfield.tsx
@@ -3,6 +3,8 @@ import { FunctionComponent } from "react";
 interface OutputFieldProps {
     output: string;
     isShown: boolean;
+    canGenerate: boolean;
+    callback: () => void;
 }
 
 const OutputField: FunctionComponent<OutputFieldProps> = (props) => {
@@ -28,9 +30,9 @@ const OutputField: FunctionComponent<OutputFieldProps> = (props) => {
                     {hasOutput ? output : "Generate a password first"}
                 </p>
             </div>
-            <div className=" block group ">
+            <div className=" flex group ">
                 <button
-                    className=" m-2 "
+                    className=" "
                     onClick={(e) => {
                         e.preventDefault();
                         if (hasOutput) {
@@ -38,11 +40,27 @@ const OutputField: FunctionComponent<OutputFieldProps> = (props) => {
                         }
                     }}
                 >
-                    <svg className={" stroke-slate-50 " + (hasOutput ? " fill-slate-800 " : " fill-slate-300 cursor-not-allowed ")}
+                    <svg className={" " + (hasOutput ? " fill-slate-800 " : " fill-slate-300 cursor-not-allowed ")}
                         xmlns="http://www.w3.org/2000/svg"
                         height="28" width="28" viewBox="0 0 48 48"
                     >
                         <path d="M9 43.95q-1.2 0-2.1-.9-.9-.9-.9-2.1V10.8h3v30.15h23.7v3Zm6-6q-1.2 0-2.1-.9-.9-.9-.9-2.1v-28q0-1.2.9-2.1.9-.9 2.1-.9h22q1.2 0 2.1.9.9.9.9 2.1v28q0 1.2-.9 2.1-.9.9-2.1.9Zm0-3h22v-28H15v28Zm0 0v-28 28Z" />
+                    </svg >
+                </button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        if (props.canGenerate) {
+                            props.callback();
+                        }
+                    }}
+                    className=" "
+                >
+                    <svg className={" " + (props.canGenerate ? " fill-slate-800 " : " fill-slate-300 cursor-not-allowed ")}
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="28" width="28" viewBox="0 0 48 48"
+                    >
+                        <path d="M24 40q-6.65 0-11.325-4.675Q8 30.65 8 24q0-6.65 4.675-11.325Q17.35 8 24 8q4.25 0 7.45 1.725T37 14.45V8h3v12.7H27.3v-3h8.4q-1.9-3-4.85-4.85Q27.9 11 24 11q-5.45 0-9.225 3.775Q11 18.55 11 24q0 5.45 3.775 9.225Q18.55 37 24 37q4.15 0 7.6-2.375 3.45-2.375 4.8-6.275h3.1q-1.45 5.25-5.75 8.45Q29.45 40 24 40Z" />
                     </svg>
                 </button>
             </div>
