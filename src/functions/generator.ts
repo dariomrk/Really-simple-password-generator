db4798939daa46ef74595cdf2cb9f1024616b5df
Dario <dariomrkonjic1@gmail.com>
2022-11-13 22:55:30 +0100

    Fixed #1

--- src/functions/generator.ts
+++ src/functions/generator.ts
@@ -1,40 +1,40 @@
 import random from "./random";
 
 const generator = (
-    useUppercase: boolean,
-    useLowercase: boolean,
-    useNumbers: boolean,
-    useSpecial: boolean,
-    length: number
+  useUppercase: boolean,
+  useLowercase: boolean,
+  useNumbers: boolean,
+  useSpecial: boolean,
+  length: number,
 ) => {
-    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lowercase = uppercase.toLowerCase();
-    const numbers = "0123456789";
-    const special = "-_()[]{}*+=<>#$%&/?!;:,.";
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercase = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const special = "-_()[]{}*+=<>#$%&/?!;:,.";
 
-    let characters = "";
+  let characters = "";
 
-    if(useUppercase) {
-        characters += uppercase;
-    }
-    if(useLowercase) {
-        characters += lowercase;
-    }
-    if(useNumbers) {
-        characters += numbers;
-    }
-    if(useSpecial) {
-        characters += special;
-    }
+  if (useUppercase) {
+    characters += uppercase;
+  }
+  if (useLowercase) {
+    characters += lowercase;
+  }
+  if (useNumbers) {
+    characters += numbers;
+  }
+  if (useSpecial) {
+    characters += special;
+  }
 
-    let output = ""
+  let output = "";
 
-    for (let i = 0; i < length; i++) {
-        const char = characters.charAt(random(characters.length));
-        output += char;
-    }
-    
-    return output;
-}
+  for (let i = 0; i < length; i++) {
+    const char = characters.charAt(random(characters.length - 1));
+    output += char;
+  }
+
+  return output;
+};
 
 export default generator;
--- src/functions/random.ts
+++ src/functions/random.ts
@@ -1,6 +1,6 @@
 const random = (maxNumber: number) => {
   while (true) {
-    let random_num = new Uint8Array(1); // 2048 = number length in bits
+    let random_num = new Uint8Array(1);
     window.crypto.getRandomValues(random_num);
 
     if (random_num[0] > maxNumber) {
